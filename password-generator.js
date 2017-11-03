61cd1f6f6dcc9837cc2029b4daf246c4d3f16d7b
dbrinkley <personal@dylanbrinkley.com>
2017-11-02 22:19:32 -0700

    copy to clipboard btn 1.1

--- password-generator.js
+++ password-generator.js
@@ -2,29 +2,28 @@ window.addEventListener("load", loadEventListeners);
 
 function loadEventListeners() {
   document.getElementById("btn").addEventListener("click", finalPassword);
-  document.getElementById("copyBtn").addEventListener("click", copy);
+  document.getElementById("copyBtn").addEventListener("click", copyTextToClipboard);
 }//end of load EventListeners function
 
 
 const alphabetNumbers = 'abcdefghijklmnopqrstuvwxyz0123456789'.split(''); //creates array with letters
 const symbols = '!"#$%&()*+,-.:;<=>?@[]^_`{|}~'.split(''); //creates array with symbols
 const capitals = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); //creates array with symbols
-
-let passwordChoices = alphabetNumbers; //arary to hold all available characters for password, starts with alphabetNumbers array
-
+let passwordChoices = alphabetNumbers; //arary to hold all available characters for password, inital is alphabetNumbers array
 
 
+//Checks if checkbox for symbols is checked then adds symbols to passwordChoices
 let addSymbols = () => {
   if(document.getElementById("symbol").checked) {
     passwordChoices = passwordChoices.concat(symbols);
   } //END if statement
 } //END addSymobls Function
 
+//Checks if checkbox for capitals is checked then adds capital letters to passwordChoices
 let addCapitals = () => {
   if(document.getElementById("captial").checked) {
    passwordChoices = passwordChoices.concat(capitals);
   } //END if statement
-
 } //END addSymobls Function
 
 
@@ -33,6 +32,7 @@ let generatePassword = () => {
   let password = [];
   let passwordLength = document.getElementById("passwordLengthInput").value; //give value for length inputed in text box
 
+  //adds characters to array depending on length requested
   while(password.length < passwordLength) {
     let random = Math.floor(Math.random() * passwordChoices.length); //gives random number
     password.push(passwordChoices[random]);
@@ -52,29 +52,23 @@ let finalPassword = () => {
 }//END of generatePassword function
 
 
-function copy() {
-  let copyText = document.getElementById("passwordDisplay");
-  // copyText.innerHTML.select();
 
-  function getSelectionHtml() {
-      var html = "";
-      if (typeof window.getSelection != "undefined") {
-          var sel = window.getSelection();
-          if (sel.rangeCount) {
-              var container = document.createElement("div");
-              for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                  container.appendChild(sel.getRangeAt(i).cloneContents());
-              }
-              html = container.innerHTML;
-          }
-      } else if (typeof document.selection != "undefined") {
-          if (document.selection.type == "Text") {
-              html = document.selection.createRange().htmlText;
-          }
-      }
-      return html;
-      html.execCommand("Copy");
+
+function copyTextToClipboard() {
+  let copyText = document.getElementById("passwordDisplay").innerHTML;
+  var textArea = document.createElement("textarea");
+
+  textArea.value = copyText;
+  document.body.appendChild(textArea);
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
   }
 
-
-}
+  document.body.removeChild(textArea);
+} //END copyTextToClipboard
